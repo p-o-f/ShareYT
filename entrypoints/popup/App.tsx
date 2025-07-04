77f443b5844095a35a7cee1b9443348e2cadc2a3
Yaniv Briker <brikeryaniv@gmail.com>
2025-07-03 17:21:15 -0700

    small

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import AuthProvider, { useAuth } from "./AuthContext";
+import AuthProvider from "./AuthContext";
+import LoginForm from "./LoginForm";
 
 export default function App() {
-  const { user, loginWithGoogle, logout } = useAuth();
-
   return (
     <AuthProvider>
-      {user?.displayName}
-      <button onClick={loginWithGoogle}>Login</button>
-      <button onClick={logout}>Login</button>
+      <LoginForm />
     </AuthProvider>
   );
 }
