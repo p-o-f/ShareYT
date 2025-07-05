67719753695b6476c0d985975aaf79cab26da44f
p-o-f <parthib.faruk@gmail.com>
2025-07-05 00:03:55 -0700

    minor

--- entrypoints/popup/AuthContext.tsx
+++ entrypoints/popup/AuthContext.tsx
@@ -54,9 +54,10 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
 
   const loginWithGoogle = async () => {
     if (isFirefoxExtension()) {
-      console.log("Performing Firefox Google login");
+      console.log("Performing Firefox Google login (AuthContext.tsx)");
       await messaging.sendMessage("auth:signInFirefox");
     } else {
+      console.log("Performing Chrome Google login (AuthContext.tsx)");
       await messaging.sendMessage("auth:signIn");
     }
   };
