91c697aef8fa15f19419ef657981c3236a98772a
parth <parthib.faruk@gmail.com>
2025-10-28 00:04:42 -0700

    Revert "modify offscreen document ctx"
    This reverts commit 515417b80c2ac1bedf116433980e0fb069f52fec.
    
    -Note to self: if want to use offscreen doc code in the future for a browser persistence context bypass, view the commit just before this reversion to reference offscreen doc setup

--- entrypoints/background/offscreenInteraction.ts
+++ entrypoints/background/offscreenInteraction.ts
@@ -3,11 +3,11 @@ declare const clients: Clients;
 import { isFirebaseUser } from '@/utils/firebase';
 import { PublicPath } from 'wxt/browser';
 
-export const OFFSCREEN_DOCUMENT_PATH = '/offscreen.html';
+const OFFSCREEN_DOCUMENT_PATH = '/offscreen.html';
 let creatingOffscreenDocument: Promise<void> | null;
 // Chrome only allows for a single offscreenDocument. This is a helper function
 // that returns a boolean indicating if a document is already active.
-export async function hasOffscreenDocument() {
+async function hasOffscreenDocument() {
   // Check all windows controlled by the service worker to see if one
   // of them is the offscreen document with the given path
   return (await clients.matchAll()).some(
@@ -15,7 +15,7 @@ export async function hasOffscreenDocument() {
   );
 }
 
-export async function ensureOffscreenDocument(path: PublicPath) {
+async function setupOffscreenDocument(path: PublicPath) {
   // If we do not have a document, we are already setup and can skip
   if (!(await hasOffscreenDocument())) {
     // create offscreen document
@@ -33,7 +33,7 @@ export async function ensureOffscreenDocument(path: PublicPath) {
   }
 }
 
-export async function closeOffscreenDocument() {
+async function closeOffscreenDocument() {
   if (!(await hasOffscreenDocument())) return;
   await browser.offscreen.closeDocument();
 }
@@ -55,7 +55,7 @@ async function getAuth() {
 
 export async function firebaseAuth() {
   try {
-    await ensureOffscreenDocument(OFFSCREEN_DOCUMENT_PATH);
+    await setupOffscreenDocument(OFFSCREEN_DOCUMENT_PATH);
     const auth = await getAuth();
     console.log('User Authenticated:', auth);
     return auth;
